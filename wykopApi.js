683cd04bb57a4edbcf0314fe52716ace1dcd734d
Karol Krupinski <karolkk01@gmail.com>
2019-11-18 20:16:39 +0100

    fix leak of api key

---
+++ wykopApi.js
@@ -0,0 +1,33 @@
+const axios = require('axios')
+require('dotenv').config();
+
+function fetchMemes(channel) {
+    const page = Math.floor(Math.random() * 10)
+
+    axios.post('https://a2.wykop.pl/Search/Entries/page/' + page + '/appkey/' + process.env.wykop_key + '?q=meme&data=compacted&return=embed', {
+    })
+    .then((res) => {
+        const item = Math.floor(Math.random() * res.data.data.length)
+        const url = res.data.data[item].embed.url
+        channel.send({files: [url]})
+    })
+    .catch((error) => {
+        channel.send('Coś się... coś się popsuło')
+    })
+}
+
+function handleWykop(message) {
+    var str = message.content.toLowerCase();
+
+    if (!str.startsWith(process.env.prefix) || message.author.bot) return false;
+    if (str.match(/meme/)) {
+        fetchMemes(message.channel)
+    } else {
+        return false;
+    }
+    return true;
+}
+
+module.exports = {
+    handleWykop: handleWykop
+};

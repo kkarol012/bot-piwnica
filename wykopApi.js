94f1d50d5e731a06f1a8318a8b24ebf400fb5de6
Karol Krupinski <karolkk01@gmail.com>
2019-11-18 22:01:45 +0100

    prevent from error if first item has no image,

--- wykopApi.js
+++ wykopApi.js
@@ -3,18 +3,25 @@ require('dotenv').config();
 
 function fetchMemes(channel) {
     const page = Math.floor(Math.random() * 10)
-
     axios.post('https://a2.wykop.pl/Search/Entries/page/' + page + '/appkey/' + process.env.wykop_key + '?q=meme&data=compacted&return=embed', {
     })
     .then((res) => {
-        const item = Math.floor(Math.random() * res.data.data.length)
-        const url = res.data.data[item].embed.url
+        const url = getMemeFromResponse(res.data.data, 0)
         channel.send({files: [url]})
     })
     .catch((error) => {
         console.log(error)
         channel.send('Coś się... coś się popsuło')
     })
+}
+
+function getMemeFromResponse(data, infLoopPrevention) {
+    let item = Math.floor(Math.random() * data.length)
+    if (data[item].embed === undefined && infLoopPrevention <= 25) {    
+        return getMemeFromResponse(data, ++infLoopPrevention)
+    }
+    return data[item].embed.url
+
 }
 
 function handleWykop(message) {

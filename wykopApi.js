9e178d93ceb48f08bdcbeae1cb67e4cc1caffc3a
Karol Krupinski <karolkk01@gmail.com>
2019-11-18 21:42:22 +0100

    Revert "add log message to debug issue"
    This reverts commit 8924200e12c36848db8b0593ce838384981485dd.

--- wykopApi.js
+++ wykopApi.js
@@ -3,12 +3,10 @@ require('dotenv').config();
 
 function fetchMemes(channel) {
     const page = Math.floor(Math.random() * 10)
-    console.log('b4 fetch')
+
     axios.post('https://a2.wykop.pl/Search/Entries/page/' + page + '/appkey/' + process.env.wykop_key + '?q=meme&data=compacted&return=embed', {
     })
     .then((res) => {
-        console.log('response:')
-        console.log(res)
         const item = Math.floor(Math.random() * res.data.data.length)
         const url = res.data.data[item].embed.url
         channel.send({files: [url]})
@@ -20,15 +18,9 @@ function fetchMemes(channel) {
 }
 
 function handleWykop(message) {
-    console.log('handleWykop: func')
-
-    console.log(process.env.prefix)
-
     var str = message.content.toLowerCase();
 
     if (!str.startsWith(process.env.prefix) || message.author.bot) return false;
-    console.log('handleWykop: after logic')
-
     if (str.match(/meme/)) {
         fetchMemes(message.channel)
     } else {

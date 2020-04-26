f54f388b69c2c289b6efc128f197b69e21cf3e84
Karol Krupinski <karolkk01@gmail.com>
2020-04-27 00:04:50 +0200

    zmieniono sposob wyszukiwania gifow

--- GiphyApi.js
+++ GiphyApi.js
@@ -2,25 +2,27 @@ const axios = require('axios')
 require('dotenv').config();
 
 function getRandomRicardoGif(channel) {
-    getGif("ricardo milos", channel);
+    getGif("ricardo_milos", channel);
 }
 function getRandomCatGif(channel) {
-    getGif("cute+cat", channel);
+    getGif("cute_cat", channel);
 }
 
 function getGif(query, channel) {
-    url = "https://api.giphy.com/v1/gifs/search";
-    const number = Math.floor(Math.random() * 999);
+    url = "https://api.giphy.com/v1/gifs/random";
+    // const number = Math.floor(Math.random() * 999);
     axios.get(url, {
         params: {
             api_key: process.env.giphy_api_key,
-            offset: number,
-            q: query,
+            // offset: number,
+            tag: query,
             limit: '1'
         }
     })
         .then((res) => {
-            url = res.data.data[0].images.original.url;
+            // console.log(res.data.image_original_url)
+            url = res.data.data.image_original_url;
+            // url = res.data.data[0].images.original.url;
             channel.send({ files: [url] });
         })
         .catch((error) => {

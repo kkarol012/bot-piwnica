13d27b0a4f1f7eec9b728a047696c298e643b49e
Karol Krupinski <karolkk01@gmail.com>
2020-04-27 00:12:08 +0200

    covid info extension

--- CovidApi.js
+++ CovidApi.js
@@ -14,9 +14,9 @@ function getStats(channel, params) {
         console.log(res.data)
         try {
             channel.send(`Obecne statystyki dla ${country}: zachorowało: ${res.data.cases}, zmarło: ${res.data.deaths}, wyzdrowiało: ${res.data.recovered}, wciąż choruje: ${res.data.active}`)
-            const deathRate = res.data.deaths / res.data.cases
-            const recoveryRate = res.data.recovered / res.data.cases
-            channel.send(`Śmiertelność: ${deathRate}, wyzdrowiało: ${recoveryRate}`)
+            const deathRate = parseFloat(res.data.deaths / res.data.cases).toFixed(2)
+            const recoveryRate = parseFloat(res.data.recovered / res.data.cases).toFixed(2)
+            channel.send(`Śmiertelność: ${deathRate}%, wyzdrowiało: ${recoveryRate}%`)
         } catch(Exception){
             channel.send('Coś się... coś się popsuło')
         }

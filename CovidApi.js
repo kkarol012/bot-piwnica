73b2d1ee3256e2e97bd3a07fa55494de45d467cd
Karol Krupinski <karolkk01@gmail.com>
2020-04-27 20:29:59 +0200

    poprawa obliczen procentów

--- CovidApi.js
+++ CovidApi.js
@@ -14,8 +14,8 @@ function getStats(channel, params) {
         console.log(res.data)
         try {
             channel.send(`Obecne statystyki dla ${country}: zachorowało: ${res.data.cases}, zmarło: ${res.data.deaths}, wyzdrowiało: ${res.data.recovered}, wciąż choruje: ${res.data.active}`)
-            const deathRate = parseFloat(res.data.deaths / res.data.cases).toFixed(2)
-            const recoveryRate = parseFloat(res.data.recovered / res.data.cases).toFixed(2)
+            const deathRate = parseFloat(res.data.deaths / res.data.cases).toFixed(2) * 100
+            const recoveryRate = parseFloat(res.data.recovered / res.data.cases).toFixed(2) * 100
             channel.send(`Śmiertelność: ${deathRate}%, wyzdrowiało: ${recoveryRate}%`)
         } catch(Exception){
             channel.send('Coś się... coś się popsuło')

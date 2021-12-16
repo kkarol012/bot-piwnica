e8c61c4ceefb2bb5ee137b4ece3b79bd9e105bff
kkarol012 <karolkk01@gmail.com>
2021-12-16 21:55:40 +0100

    fix: reintegrate with giphy api

--- GiphyApi.js
+++ GiphyApi.js
@@ -1,32 +1,33 @@
-const axios = require('axios')
-require('dotenv').config();
+const axios = require("axios");
+require("dotenv").config();
 
 function getRandomRicardoGif(channel) {
-    getGif("ricardo_milos", channel);
+  getGif("ricardo_milos", channel);
 }
 function getRandomCatGif(channel) {
-    getGif("cute_cat", channel);
+  getGif("cute_cat", channel);
 }
 
 function getGif(query, channel) {
-    url = "https://api.giphy.com/v1/gifs/random";
-    axios.get(url, {
-        params: {
-            api_key: process.env.giphy_api_key,
-            tag: query,
-            limit: '1'
-        }
+  url = "https://api.giphy.com/v1/gifs/random";
+  axios
+    .get(url, {
+      params: {
+        api_key: process.env.giphy_api_key,
+        tag: query,
+        limit: "1",
+      },
     })
-        .then((res) => {
-            url = res.data.data.image_original_url;
-            channel.send({ files: [url] });
-        })
-        .catch((error) => {
-            console.log(error);
-            channel.send('Coś się... coś się popsuło');
-        });
+    .then((res) => {
+      url = res.data.data.embed_url;
+      channel.send(url);
+    })
+    .catch((error) => {
+      console.log(error);
+      channel.send("Coś się... coś się popsuło");
+    });
 }
 module.exports = {
-    getRandomRicardoGif: getRandomRicardoGif,
-    getRandomCatGif: getRandomCatGif
+  getRandomRicardoGif: getRandomRicardoGif,
+  getRandomCatGif: getRandomCatGif,
 };
